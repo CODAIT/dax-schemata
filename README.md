1ba5a4f6c4f54f7eaf42fa33f709e2c20dd94b0f
Hong Xu <hongx@ibm.com>
2020-12-08 15:02:32 -0800

    Add tests for validating schema files. (#4)
    * Add tests for validating schema files.
    
    Run lint programs and try to use the latest PyDAX to load each dataset
    once.
    
    * Missing space
    
    * Missing license header in tox.ini
    
    * Update tox.ini
    
    Co-authored-by: Brendan Dwyer <brendan.dwyer@ibm.com>
    
    * Update tox.ini
    
    Co-authored-by: Brendan Dwyer <brendan.dwyer@ibm.com>
    
    * Only run flake8 on the tests dir
    
    * Add some assertions to ensure that dataset names and versions match so that pydax doesn't miss any datasets during this test
    
    * Load the local files instead of trying to load remote ones
    
    Otherwise this script won't be testing the new changes
    
    * Sanity check
    
    Co-authored-by: Brendan Dwyer <brendan.dwyer@ibm.com>

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 This is the repository that we maintain the default DAX schema files for
 [PyDAX](https://github.com/codait/pydax).
+
+To run tests locally, run:
+
+    $ pip install -U -r requirements/tox.txt  # If you are inside a virtual environment, conda environment
+    $ pip3 install --user -U tox  # If you are outside any virtual environment or conda environment
+
+    # lint tests
+    $ tox -e lint
+    # pydax tests
+    $ tox -e pydax
---
+++ tests/test_pydax.py
@@ -0,0 +1,48 @@
+#!/bin/env python
+
+#
+# Copyright 2020 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This scripts tests all files in CODAIT/dax-schemata. It shouldn't report any error.
+
+import yaml
+
+import pydax
+
+
+pydax.init(
+    DEFAULT_DATASET_SCHEMA_URL='datasets.yaml',
+    DEFAULT_FORMAT_SCHEMA_URL='formats.yaml',
+    DEFAULT_LICENSE_SCHEMA_URL='licenses.yaml'
+)
+
+with open('datasets.yaml') as f:
+    datasets = yaml.safe_load(f)
+
+# Datasets name are the same from the schema files. This helps ensure that PyDAX doesn't miss any dataset during the
+# test.
+assert frozenset(datasets['datasets']) == frozenset(pydax.list_all_datasets())
+# Sanity check. In case of all tests being skipped because of a minor error such as in formatting.
+assert len(pydax.list_all_datasets()) > 0
+
+for name, versions in pydax.list_all_datasets().items():
+    # Versions must be the same from the schema files. This helps ensure that PyDAX doesn't miss any dataset during the
+    # test.
+    assert frozenset(datasets['datasets'][name]) == frozenset(versions)
+    # Sanity check. In case of all tests being skipped because of a minor error such as in formatting.
+    assert len(versions) > 0
+    for version in versions:
+        pydax.load_dataset(name=name, version=version, subdatasets=None)
